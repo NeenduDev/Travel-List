896c8319756f69f2593f58cecfe7d410740b81f5
NeenduDev <neendu.wickremasinghe@gmail.com>
2024-12-24 23:57:14 +0530

    Add initial layout structure for the application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 export default function App() {
-  return <h1>Far Away</h1>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <StackingList />
+      <Stats />
+    </div>
+  );
 }
 
-function Logo() {}
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
 
-function Form() {}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
 
-function StackingList() {}
+function StackingList() {
+  return <div className="list">LIST</div>;
+}
 
-function Stats() {}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
